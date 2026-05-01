a5fa924d01dfe202c4059a440125bd1dd80637e4
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-02 02:56:17 +0500

    start doing that, other way

--- src/main.py
+++ src/main.py
@@ -1,86 +1,17 @@
-import random
-from enemies import SmartEnemy
-
-player_hp = 40
-block = 0
-
-enemy = SmartEnemy("Mr.Squidward", 40)
-is_playing = True
 
 
-def enemy_attack_player(block, player_hp):
-    damage = random.randint(4, 9)
-
-    damage = max(0, damage - block)
-    block = 0
-
-    player_hp -= damage
-    player_hp = max(0, player_hp)
-
-    return block, player_hp, damage
 
 
-def player_block():
-    return random.randint(4, 6)
 
 
-def skip_turn():
-    print("You skipped turn")
+print("\n=========================================")
+print("Welcome to the Slay The Terminal")
+print("It is a fanmade game, so have fun")
+print("=========================================\n")
 
-
-while is_playing:
-    print("======================")
-    print(f"{enemy.name}'s HP: {enemy.hp}")
-    print(f"Your HP: {player_hp}")
-    print(f"Block: {block}")
-    print("======================")
-    print("q. Quit")
-    print("0. Skip turn")
-    print("1. Attack (6 dmg)")
-    print("2. Block")
-    print("======================")
-
-    choice = input("Choice: ").lower()
-
-    # ================= ATTACK =================
-    if choice == "1":
-        enemy.hp -= 6
-        enemy.hp = max(0, enemy.hp)
-
-        if enemy.hp == 0:
-            print(f"{enemy.name} was killed")
-            print("You won")
-            break
-
-        block, player_hp, damage = enemy_attack_player(block, player_hp)
-
-        print(f"You attacked {enemy.name}. HP left: {enemy.hp}")
-        print(f"He dealt {damage} damage.")
-
-    # ================= SKIP =================
-    elif choice == "0":
-        skip_turn()
-        block, player_hp, damage = enemy_attack_player(block, player_hp)
-        print(f"{enemy.name} dealt {damage} damage.")
-
-    # ================= BLOCK =================
-    elif choice == "2":
-        block += player_block()
-        print("You raised block")
-
-        block, player_hp, damage = enemy_attack_player(block, player_hp)
-
-        print(f"Block now: {block}")
-        print(f"{enemy.name} dealt {damage} damage.")
-
-    # ================= QUIT =================
-    elif choice == "q":
-        print("You ran away")
-        break
-
-    # ================= DEATH =================
-    if player_hp == 0:
-        print("You DIED")
-        break
-
-print("Game over")
+name = input("Enter your name: ")
+name = name.strip()
+while not name:
+    print("Name cannot be empty bro...")
+    name = input("Enter your name: ")
+    name = name.strip()
--- src/player.py
+++ src/player.py
@@ -1,18 +1,25 @@
-#player.py
 class Player:
-    def __init__(self, hp=50):
-        self.max_hp = hp
-        self.hp = hp
-        self.block = 0
+    def __init__(self,name,hp = 50,block = 0, damage = 6):
+        self.name = name #just name
+        self.hp = hp #base hp
+        self.max_hp = hp #max_hp, capped at 50 for now
+        self.block = block # base shield
+        self.damage = damage # base atk
 
-    def take_damage(self, damage):
-        reduced = max(0, damage - self.block)
-        self.block = 0
-        self.hp = max(0, self.hp - reduced)
-        return reduced
+    def deal_damage(self,target):
+        pass
 
-    def gain_block(self, amount):
-        self.block += amount
+    
+    def take_damage(self,amount):
+        damage = max(0, amount - self.block)
+        self.hp = max(0, self.hp - damage)
+        self.block = max(0,self.block-amount)
+
+    def add_block(self,amount):
+        self.block += amount #gain 'amount' of block
+    
+    def skip_turn(self):
+        print("You just skipped the turn")
 
     def is_alive(self):
         return self.hp > 0
