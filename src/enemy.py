ae4a5a555c2ef52ff23fced8030a38c428fb6bef
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-01 23:05:34 +0500

    adding mechanics for playuer and an enemy

--- src/enemy.py
+++ src/enemy.py
@@ -1,1 +1,20 @@
 #enemy.py
+import random
+
+class Enemy:
+    def __init__(self, name="Slime", hp=30):
+        self.name = name
+        self.hp = hp
+        self.block = 0
+
+    def take_damage(self, damage):
+        reduced = max(0, damage - self.block)
+        self.block = 0
+        self.hp = max(0, self.hp - reduced)
+        return reduced
+
+    def attack_damage(self):
+        return random.randint(4, 8)
+
+    def is_alive(self):
+        return self.hp > 0
--- src/player.py
+++ src/player.py
@@ -1,1 +1,18 @@
 #player.py
+class Player:
+    def __init__(self, hp=50):
+        self.max_hp = hp
+        self.hp = hp
+        self.block = 0
+
+    def take_damage(self, damage):
+        reduced = max(0, damage - self.block)
+        self.block = 0
+        self.hp = max(0, self.hp - reduced)
+        return reduced
+
+    def gain_block(self, amount):
+        self.block += amount
+
+    def is_alive(self):
+        return self.hp > 0
