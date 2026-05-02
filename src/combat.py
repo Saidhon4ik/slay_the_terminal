e4a278b04224e4ecc3500e928e5c7798109bc02f
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-02 11:39:36 +0500

    So basically , i messed up and... I have a few bugs, i just wanna update those two files just in case I'd delete my windows again, so i can fix them a bit later as I am leaving for now, cuz i am tired already. Now I understand that meme about debugging... I never understood that joke before but now I see the reason before these memes about it

--- src/combat.py
+++ src/combat.py
@@ -1,34 +1,27 @@
 from player import Player
 from enemy import Enemy
 
-
 def start_combat(player, enemy):  # passing the objects
     while player.is_alive() and enemy.is_alive():
-        player.block = 0
-        enemy.block = 0
         print("\n================================")
         print(f"Player's hp: {player.hp}/{player.max_hp}")
-        print(f"Player's current block: {player.block}")
-        print("================================\n")
-
-        print("\n================================")
+        print("================================")
         print(f"Enemy's hp: {enemy.hp}/{enemy.max_hp}")
         print(enemy.get_intent())
-        print("\n================================")
-
-        print("\n================================")
-        print("\nWhat do you do?")
+        print("================================")
+        print("What do you do?")
         print("1. Attack")
         print("2. Block")
         print("3. Skip")
         choice = input("> ")
-        print("================================\n")
+        print("================================")
 
         if choice == "1":
             player.deal_damage(enemy)
         
         elif choice == "2":
             player.add_block(5)
+            print(f"Player's current block: {player.block}")
 
         elif choice == "3":
             player.skip_turn()
@@ -38,20 +31,27 @@ def start_combat(player, enemy):  # passing the objects
 
 
         if not enemy.is_alive():
-            print("\n================================")
+            print("================================")
             print(f"{enemy.name} was defeated by a {player.name}")
             print("You have won the battle")
             print("You WON")
-            print("================================\n")
+            print("================================")
             break
+        
 
-        enemy.take_action(player)
-
+        print(f"Enemy block before attack: {enemy.block}")
+        enemy.take_action(player)   
+        print(f"Enemy block before attack: {enemy.block}")
 
         if not player.is_alive():
-            print("\n================================")
+            print("================================")
             print(f"{player.name} was defeated by a {enemy.name}")
             print("You have lost the battle")
             print("You LOST")
-            print("================================\n")
+            print("================================")
             break
+
+
+
+        player.block = 0
+        enemy.block = 0
--- src/enemy.py
+++ src/enemy.py
@@ -10,14 +10,14 @@ class Enemy:
     def take_damage(self,amount):
         damage = max(0,amount - self.block)
         self.hp = max(0,self.hp - damage)
-        self.block = max(0,self.block - amount)
+        self.block = max(0, self.block - amount)
 
 
     def is_alive(self):
         return self.hp > 0
     
     def get_intent(self):
-        return f"{self.name} intends to attck you for {self.damage} damage"
+        return f"{self.name} intends to attack you for {self.damage} damage"
     
 
     def take_action(self,target):
@@ -26,9 +26,7 @@ class Enemy:
             target.take_damage(self.damage)
             print("\n=========================================================================")
             print(f"{self.name} decided to attack you and deal {self.damage} damage to you")
-            print("=========================================================================\n")
         elif action == "block":
             self.block = random.randint(4,6)
             print("\n=========================================================================")
             print(f"{self.name} decided to block and gains {self.block} block")
-            print("=========================================================================\n")
