14ea40e010863e4bafbb167b07896936fbe4936a
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-03 03:37:24 +0500

    CAAAAAAAAAAAARDS

---
+++ src/cards.py
@@ -0,0 +1,7 @@
+#WELL WELL WELL, CARDS!!!! FINALLY
+
+class Cards:
+    def __init__(self,name,cost,description):
+        self.name = name
+        self.cost = cost
+        self.description = description
