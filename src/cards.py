20095b911baed5b16ae9753e0002024075fd337d
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-03 04:20:40 +0500

    added two cards for now, strike and defend with description

--- src/cards.py
+++ src/cards.py
@@ -5,3 +5,19 @@ class Cards:
         self.name = name
         self.cost = cost
         self.description = description
+
+class Strike(Cards):
+    def __init__(self):
+        super().__init__("Strike",1,"Deals 8 damage")
+    
+    def play(self,user,target):
+        target.take_damage(8)
+
+
+class Defend(Cards):
+    def __init__(self):
+        super().__init__("Block",1,"Blocks 8 upcoming damage and will be reset each turn")
+        
+    
+    def play(self,user,target):
+        user.add_block(8)
