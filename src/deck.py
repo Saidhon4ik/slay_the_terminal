dfe88d072133aa26972fb008350324e131c87334
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-03 04:27:35 +0500

    added shuffling between hand, discard pile and cards(next cards as hand will be empty)

--- src/deck.py
+++ src/deck.py
@@ -10,7 +10,7 @@ class Deck:
         self.discard = []
 
     def draw(self,n):
-        for i in range(n):
+        for _ in range(n):
             if not self.cards:
                 break
             card = self.cards.pop(0)
@@ -19,3 +19,10 @@ class Deck:
     def discard_hand(self):
         self.discard.extend(self.hand)
         self.hand = []
+    
+
+    def reshuffle(self):
+        if not self.cards:
+            self.cards = self.discard
+            random.shuffle(self.cards)
+            self.discard = []
