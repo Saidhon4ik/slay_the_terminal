db1e7a0a80c074f3ac80fe94eca1988aa819e71e
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-03 04:20:00 +0500

    for now just added draw, and all of this stuff

---
+++ src/deck.py
@@ -0,0 +1,21 @@
+from cards import Strike, Defend
+import random
+
+
+class Deck:
+    def __init__(self):
+        self.cards = [Strike(), Strike(), Strike(), Defend(), Defend(), Defend()]
+        random.shuffle(self.cards)
+        self.hand = []
+        self.discard = []
+
+    def draw(self,n):
+        for i in range(n):
+            if not self.cards:
+                break
+            card = self.cards.pop(0)
+            self.hand.append(card)
+    
+    def discard_hand(self):
+        self.discard.extend(self.hand)
+        self.hand = []
